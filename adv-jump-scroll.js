713c20c7b01edddb2e01455ab2881c7050c33061
hotwag <mhotwagner@gmail.com>
2015-08-05 14:14:03 -0400

    Added simple init handling, used on fluentcity.com'

--- adv-jump-scroll.js
+++ adv-jump-scroll.js
@@ -9,3 +9,22 @@ function scrollToElement(el, options) {
 	$('body').animate({scrollTop:y}, duration)
 }
 
+function initJumpScroll() {
+	// Just to stick this in one place
+	$('a[href^="#"]').on('click', function(e) {
+		// Fail out if this is just an empty link
+		if ($(this).attr('href').length == 1) return true;
+		e.preventDefault();
+
+		// grab the selector
+		sel = $(this).attr('href').substr(1);
+
+		// Identify what we're srolling to, name first
+		target = $('a[name="'+sel+'"]').exists() || $('a[id="'+sel+'"]').exists();
+		if (!target) {
+			console.log('WARNING! ' + $(this) + ' has no related link');
+			return false;
+		}
+		scrollToElement(target[0],{offset:130});
+	});
+}
